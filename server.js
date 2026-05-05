dc900f8383e64be029d3735e75b826a95c6e3885
KabAyrat <kabirov.ayrat2004@gmail.com>
2026-05-05 15:50:03 +0300

    написал красивый readme

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT; // берем данные из .env для настройки бд. тут хранится порт(он нам еще понадобится)
 
 // здесь берем данные из .env для подключения к бд
 const pool = new Pool({
@@ -22,7 +22,8 @@ app.use(express.static(__dirname));
 // тестовый вывод данных 
 app.get('/api/data', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM your_table LIMIT 50');
+        const result = await pool.query('SELECT * FROM users LIMIT 50');
+        
         res.json(result.rows);
     } catch (error) {
         res.status(500).json({ error: 'Ошибка' });
@@ -30,6 +31,7 @@ app.get('/api/data', async (req, res) => {
 });
 
 app.listen(PORT, () => {
+    console.log('ну чета выводится')
     console.log(`Сервер на http://localhost:${PORT}`);  
     // здесь просто выводим в консоль, для того чтоб убедиться, что бд подключена
 });
